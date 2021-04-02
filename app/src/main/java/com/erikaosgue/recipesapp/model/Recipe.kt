8bc89d4e04bbdce67638999cf30cdf17affa837c
erikaosgue <1476@holbertonschool.com>
2021-04-01 23:51:11 -0500

    Add model file of the recipe object

---
+++ app/src/main/java/com/erikaosgue/recipesapp/model/Recipe.kt
@@ -0,0 +1,25 @@
+package com.erikaosgue.recipesapp.model
+
+
+//class Recipe(var title: String, var description: String, var image: String)
+
+//data class Recipe(var title: String, var description: String, var image: String)
+
+//class Recipe {
+//    var title: String? = null
+//    var description: String? = null
+//    var image: String? = null
+//}
+
+class Recipe {
+    lateinit var title: String
+    lateinit var ingredients: String
+    lateinit var image: String
+    lateinit var link: String
+
+    override fun toString(): String {
+        return "Recipe(title=$title, ingredients=$ingredients, image=$image, link=$link"
+    }
+}
+
+
