add44a928dc9b27aa9c6db6d4c235a1c0d493881
erikaosgue <1476@holbertonschool.com>
2021-04-01 23:53:23 -0500

    -Add the data/RecipeAdapter, to be able to use the recyclerview

---
+++ app/src/main/java/com/erikaosgue/recipesapp/data/RecipeAdaper.kt
@@ -0,0 +1,81 @@
+package com.erikaosgue.recipesapp.data
+
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.erikaosgue.recipesapp.R
+import com.erikaosgue.recipesapp.ShowLinkActivity
+import com.erikaosgue.recipesapp.model.Recipe
+import com.squareup.picasso.Picasso
+
+class RecipeAdapter(private val context: Context, private val listRecipes: ArrayList<Recipe>):
+    RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder>(){
+
+    // Class viewHolder, that
+    inner class RecipeViewHolder(var view: View): RecyclerView.ViewHolder(view) {
+
+        // Instantiating the views from the view (row_list) inflated that will represent our viewHolder
+
+        private val titleRecipe = view.findViewById<TextView>(R.id.titleRecipeId)
+        private val imageRecipe = view.findViewById<ImageView>(R.id.imageRecipeId)
+        private val ingredientsRecipe = view.findViewById<TextView>(R.id.ingredientsRecipeId)
+        private val moreInfoButton = view.findViewById<Button>(R.id.moreInfoButton)
+
+
+
+
+        // bindData receives an Object of type Recipe
+        fun bindData(recipe: Recipe) {
+
+            titleRecipe.text = recipe.title
+            ingredientsRecipe.text = recipe.ingredients
+
+            //Adding the image using the Picasso Library
+            if (recipe.image != "") {
+                Picasso.get()
+                    .load(recipe.image)
+                    .placeholder(android.R.drawable.ic_menu_report_image)
+                    .error(android.R.drawable.ic_menu_report_image)
+                    .resize(600, 500)
+                    .into(imageRecipe)
+            } else {
+                Picasso.get()
+                    .load(R.mipmap.ic_launcher).into(imageRecipe)
+            }
+
+            moreInfoButton.setOnClickListener {
+                val intent = Intent(context, ShowLinkActivity::class.java)
+                intent.putExtra("link", recipe.link)
+                context.startActivity(intent)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
+
+        //Inflates the viewHolder (the hole row_list.xml file) in one Object
+        val viewInflated = LayoutInflater.from(parent.context).inflate(R.layout.row_list , parent, false)
+        return RecipeViewHolder(viewInflated)
+    }
+
+    override fun onBindViewHolder(recipeViewHolder: RecipeViewHolder, position: Int) {
+        recipeViewHolder.bindData(listRecipes[position])
+    }
+
+    override fun getItemCount(): Int {
+        return listRecipes.size
+    }
+
+
+
+
+}
+
